fc42f2b55e93ab9647627fd50f155f2d035c016a
jdamascoty <57690300+jdamascoty@users.noreply.github.com>
2020-03-21 17:06:32 -0400

    Update ViewItineraryController.java

--- src/main/java/edu/drexel/TrainDemo/controllers/ViewItineraryController.java
+++ src/main/java/edu/drexel/TrainDemo/controllers/ViewItineraryController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import edu.drexel.TrainDemo.services.ItineraryServicer;
+import edu.drexel.TrainDemo.service.ItineraryServicer;
 
 @Controller
 public class ViewItineraryController {
