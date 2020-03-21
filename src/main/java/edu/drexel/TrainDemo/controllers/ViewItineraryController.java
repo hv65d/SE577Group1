14a828ed2484a159f1415c103e0bdeca277c3294
AdithyaVadrevu <adithya.vadrevu@gmail.com>
2020-03-21 03:16:25 -0400

    Add to cart and View
    Add to cart and View Items

---
+++ src/main/java/edu/drexel/TrainDemo/controllers/ViewItineraryController.java
@@ -0,0 +1,25 @@
+package edu.drexel.TrainDemo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import edu.drexel.TrainDemo.services.ItineraryServicer;
+
+@Controller
+public class ViewItineraryController {
+
+	@Autowired
+	private ItineraryServicer itineraryServicer;
+
+	@RequestMapping("/Itinerary")
+	public ModelAndView viewItinerary() {
+
+		System.out.print("HI");
+		ModelAndView mvw = new ModelAndView();
+		mvw.setViewName("Itinerary.jsp");
+		mvw.addObject("itinerary", itineraryServicer.returnItinerary("aditya"));
+		return mvw;
+	}
+}
